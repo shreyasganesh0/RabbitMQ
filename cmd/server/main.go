a54c56344ebb8fb72edb98febb30a83633e53f6b
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-06-11 00:16:07 -0400

    Create signal handling for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+   "fmt" 
+   "os"
+   "os/signal"
+   "syscall"
+   amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conn_url := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(conn_url);
+	if (err != nil) {
+
+		fmt.Printf("Error connecting to amq: %v\n");
+		return;
+	}
+	defer conn.Close();
+
+	fmt.Printf("Connection started\n");
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT);
+
+	sig := <-sigs;
+
+	fmt.Printf("Received signal %v\n shutting down connections", sig);
+
 }
