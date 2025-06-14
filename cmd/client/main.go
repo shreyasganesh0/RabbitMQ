d6a14e65cc85959e9da9abe163af2c977c934e32
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-06-14 00:20:57 -0400

    Add queue declare and binding functionality

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,4 +4,29 @@ import "fmt"
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	conn_url := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(conn_url);
+	if (err != nil) {
+
+		fmt.Printf("Error connecting to amq: %v\n", err);
+		return;
+	}
+	defer conn.Close();
+
+	chann, err_ch := conn.Channel();
+	if (err_ch != nil) {
+
+		fmt.Printf("Error connecting to amq: %v\n", err_ch);
+		return;
+	}
+
+	fmt.Printf("Connection started\n");
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT);
+
+	sig := <-sigs;
+
+	fmt.Printf("Received signal %v\n shutting down connections", sig);
 }
--- internal/pubsub/publisher.go
+++ internal/pubsub/publisher.go
@@ -30,3 +30,43 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	return nil;
 
 }
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType int, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+
+	var queue amqp.Queue
+	var err error
+
+	chann, err_chan := conn.Channel();
+	if (err_chan != nil) {
+
+		return nil, queue, err_chan;
+	}
+
+	var durable, auto_delete, exclusive, no_wait bool;
+
+	if (simpleQueueType == 0) {
+
+		durable = true;
+	} else if (simpleQueueType == 1 {
+
+		auto_delete = true;
+		exclusive = false;
+	}
+
+	queue, err_q = chann.QueueDeclare(queueName, durable, auto_delete, exclusive, no_wait, nil)
+	if (err_q != nil) {
+
+		return chann, queue, err_q;
+	}
+
+	err_q = chann.QueueBind(queueName, key, exchange, no_wait, nil)
+
+	return chann, queue, err_q;
+
+}
